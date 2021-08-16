0b6415876a6825da75804fb1964d2ebeefb2f04b
Wei Jie <tanweijie.91@gmail.com>
2021-08-16 09:26:02 +0800

    random text and colour function created

--- script.js
+++ script.js
@@ -4,6 +4,23 @@
 let timer;
 let timeLeft = 10;    
 
+function randomColour(){
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let colour = `rgb(${red},${green},${blue})`;
+    return colour;
+}
+
+document.querySelector("span").style.color = randomColour();
+
+function randomText(){
+    const text = ["RED", "GREEN", "BLUE", "YELLOW"];
+    let i = Math.floor(Math.random() * 4);
+    return text[i];
+}
+
+document.querySelector("span").innerText = randomText();
 
 function updateTimer(){
 
