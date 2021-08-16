4e1cefe1d4e20ca8431df83c6125d4eebad325e6
Wei Jie <tanweijie.91@gmail.com>
2021-08-16 10:26:17 +0800

    basic game mechanics

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//game timer
+
 let timer;
 let timeLeft = 10;    
 
@@ -20,24 +20,47 @@ function randomText(){
     return text[i];
 }
 
-document.querySelector("span").innerText = randomText();
 
+
+//Main game mechanics
+const colourButtons = document.querySelector(".answers");
+colourButtons.addEventListener("click",(e) =>{  
+    if (e.target.className === "colour-btn"){
+        if (e.target.id === document.querySelector("span").innerText){
+            console.log("Correct!");
+            document.querySelector("span").innerText = randomText();
+            document.querySelector("span").style.color = randomColour();
+        }
+        else {
+            console.log("Wrong!");
+        }    
+    }
+});
+
+
+//Game timer
 function updateTimer(){
 
     if(timeLeft > 0){
         timeLeft -= 1;
         console.log(timeLeft);
+        document.querySelector("#time-left").innerText = `Time Left : ${timeLeft}`;
     }
-    else{
-        console.log("game over!");
-        clearInterval(timer);
-    }
+    else
+        quit();    
 }
 
+//Start function
 function start(){
     timer = setInterval(updateTimer,1000);
     updateTimer();
 }
 
-start();
+document.querySelector("#play").addEventListener("click",start);
 
+//Quit function
+function quit(){
+    console.log("game over!");
+    document.querySelector("h1").innerHTML = "GAME OVER!";
+    clearInterval(timer);
+}
